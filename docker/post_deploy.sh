a4a1248af5cfeae00f1f5eb2ad2352288afbfdaa
Mohamed Nagy <mohamed.nagy@aboutyou.com>
2021-10-24 11:45:48 +0200

    test apache setup

--- docker/post_deploy.sh
+++ docker/post_deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
-sed -i "s/80/$PORT/g" /etc/apache2/sites-available/app.conf /etc/apache2/ports.conf
+echo "host port is: $PORT"
+
+sed -i "s/80/${PORT}/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
 
 cat /etc/apache2/sites-available/app.conf
 cat /etc/apache2/ports.conf
+
 a2enmod rewrite
+
 service apache2 restart
