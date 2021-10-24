1ac5c4338ac7737dcc7cd23d3b6925be9fab022e
Mohamed Nagy <mohamed.nagy@aboutyou.com>
2021-10-24 11:05:09 +0200

    replace nginx by apache

--- docker/post_deploy.sh
+++ docker/post_deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-RUN sed -i "s/80/$PORT/g" /etc/apache2/sites-available/app.conf /etc/apache2/ports.conf
-RUN a2enmod rewrite
-RUN service apache2 restart
+sed -i "s/80/$PORT/g" /etc/apache2/sites-available/app.conf /etc/apache2/ports.conf
+
+cat /etc/apache2/sites-available/app.conf
+cat /etc/apache2/ports.conf
+a2enmod rewrite
+service apache2 restart
