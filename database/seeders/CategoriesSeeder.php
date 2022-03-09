e70aedb6a7dfb9355cc6311719cbd646a876f235
Mohamed Nagy <mohamed.nagy@aboutyou.com>
2022-03-09 09:52:09 +0100

    refactor categories

--- database/seeders/CategoriesSeeder.php
+++ database/seeders/CategoriesSeeder.php
@@ -23,15 +23,15 @@ class CategoriesSeeder extends Seeder
 
         $categories = json_decode(file_get_contents(__DIR__. '/categories.json'), true);
         foreach ($categories as $category) {
-            $image = resource_path("services/$category[id].jpeg");
-            $path = Storage::disk($disk)->putFileAs('categories', $image, "$category[id].jpeg");
+            $image = resource_path("services/$category[slug].jpeg");
+            $path = Storage::disk($disk)->putFileAs('categories', $image, "$category[slug].jpeg");
 
             $category['slug'] = Str::arSlug($category['name']);
             $category['photo'] = Storage::disk($disk)->url($path);
 
-            $cat = Category::where('id', $category['id'])->first();
+            $cat = Category::where('slug', $category['slug'])->first();
             if ($cat) {
-                Category::where('id', $cat->id)->update($category);
+                Category::where('slug', $cat->slug)->update($category);
             } else {
                 Category::create($category);
             }
